b4bdfb8cf29a246ca535924af8cee59ad864ed1e
ALX <578520080@qq.com>
2021-12-20 09:42:40 +0800

    first commit

---
+++ src/test/java/com/starblog/StarblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
